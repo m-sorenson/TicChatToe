09cc6febfa0d8bedaaec5db06d38b6913283f339
m-sorenson <m.sorenson407@gmail.com>
2015-08-06 00:20:49 -0600

    GenServer match maker and basic functioning states.

---
+++ lib/tic_chat_toe/match_maker.ex
@@ -0,0 +1,44 @@
+defmodule TicChatToe.MatchMaker do
+  use GenServer
+
+  @server __MODULE__
+
+  ## Client API
+  def find_match(socket) do
+    GenServer.cast(@server, {:find_match, socket})
+  end
+
+  def get_state() do
+    GenServer.call(@server, :get_state)
+  end
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], [name: @server])
+  end
+
+  ## Callbacks
+  def init(_args) do
+    state = []
+    {:ok, state}
+  end
+
+  def handle_cast({:find_match, socket}, []) do
+    {:noreply, [socket]}
+  end
+
+  def handle_cast({:find_match, _socket}, _state) do
+    {:noreply, []}
+  end
+
+  def handle_cast(_cast, state) do
+    {:noreply, state}
+  end
+
+  def handle_call(:get_state, {_from, _ref}, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(_call, {_from, _ref}, state) do
+    {:noreply, state}
+  end
+end
