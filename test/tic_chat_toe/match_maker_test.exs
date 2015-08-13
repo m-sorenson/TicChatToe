f9b99b56355bce18742630861b3bb6b6f2acc39e
Michael Sorenson <m.sorenson407@gmail.com>
2015-08-12 21:27:37 -0600

    Fixed broken tests, to display correct behaviour.

--- test/tic_chat_toe/match_maker_test.exs
+++ test/tic_chat_toe/match_maker_test.exs
@@ -1,15 +1,11 @@
 defmodule TicChatToe.MatchMakerTest do
   use ExUnit.Case, async: true
 
-  setup do
-    {:ok, _} = TicChatToe.MatchMaker.start_link();
-    :ok
-  end
-
   test "MatchMaker state changes" do
     socket = %Phoenix.Socket{}
     TicChatToe.MatchMaker.find_match(socket)
     assert [socket] == TicChatToe.MatchMaker.get_state()
+
     other_socket = %Phoenix.Socket{}
     TicChatToe.MatchMaker.find_match(other_socket)
     assert [] == TicChatToe.MatchMaker.get_state()
