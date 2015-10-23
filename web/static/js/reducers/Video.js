b7fd9b356cc5c27de3174d79c01cf6cd0372e0d4
Michael Sorenson <michael.sorenson@tcnbroadcasting.com>
2015-10-23 17:00:44 -0600

    Removed some webrtc spefic stuff.

--- web/static/js/reducers/Video.js
+++ web/static/js/reducers/Video.js
@@ -1,11 +1,10 @@
-import { FOUND_MATCH, GOT_DESC, ADD_SDP, ADD_CANDIDATE, ADD_PEER, ADD_STREAM, LOCAL_VIDEO, STRANGER_VIDEO, START_CALL } from '../constants/ActionTypes.js';
+import { FOUND_MATCH, ADD_PEER, ADD_STREAM, LOCAL_VIDEO, STRANGER_VIDEO } from '../constants/ActionTypes.js';
 import { RTCPeerConnection } from 'webrtc-adapter';
 let config = require('../IceServer.js');
 let _ = require('lodash')
 
 const initalState = {
   startCall: false,
-  //peerConn: new RTCPeerConnection(config),
   peerConn: undefined,
   you: {
     stream: undefined,
@@ -40,30 +39,9 @@ export default function Video(state = initalState, action ) {
               videoSrc: window.URL.createObjectURL(action.stream)
             }
           })
-    //case START_CALL:
-    //  let next_state = { ...state, startCall: action.value }
-    //  console.log('Setting startCall value to ', next_state);
-    //  return next_state
     case FOUND_MATCH:
       let peer = new RTCPeerConnection(config)
       return {...state, peerConn: peer}
-    case ADD_CANDIDATE:
-      let candidate = new RTCIceCandidate(msg.candidate)
-      peer = state.peerConn
-      peer.addIceCandidate(candidate)
-      return {...state, peerConn: peer }
-    case ADD_SDP:
-      let desc = new RTCSessionDescription(action.sdp)
-      peer = state.peerConn
-      peer.setRemoteDescription(desc)
-      console.log('set SDP');
-      console.log(desc)
-      return {...state, peerConn: peer }
-    case GOT_DESC:
-      peer = state.peerConn
-      console.log('set local desc');
-      peer.setLocalDescription(action.desc);
-      return {...state, peerConn: peer, startCall: true }
     default:
       return state;
   }
